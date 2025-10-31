7b6aa28cb28da678b6cec9f61beac27a110b864e
dishitataneja <dishitataneja@gmail.com>
2025-10-31 16:28:15 +0530

    .

--- array3/maxsub.java
+++ array3/maxsub.java
@@ -1,7 +1,7 @@
 package array3;
 
 public class maxsub {
-    public int maxSubArray(int[] nums) {
+    static int maxSubArray(int[] nums) {
         int c=0;
         int m=Integer.MIN_VALUE;
         for (int i=0;i<nums.length;i++){
@@ -11,7 +11,7 @@ public class maxsub {
         return m;
     }
     public static void main(String[] args) {
-        int[] nums={5,4,-1,7,8};
+        int[] nums={-2,1,-3,4,-1,2,1,-5,4};
         System.out.println(maxSubArray(nums));
     }
 }
