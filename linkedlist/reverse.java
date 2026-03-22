5a2e76223d4c833d3bc1226e7a092ae234d1971c
dishitataneja <dishitataneja@gmail.com>
2026-03-22 18:24:32 +0530

    add this to

--- linkedlist/reverse.java
+++ linkedlist/reverse.java
@@ -10,6 +10,27 @@ public class reverse {
             this.next = null;
         }
     }
+    //  public static Node reveseList(Node head) {
+    //     // Base case: If the list is empty or has only one node, it's already reversed.
+    //     if (head == null || head.next == null) {
+    //         return head;
+    //     }
+
+    //     // Recursive call: Reverse the rest of the list starting from the second node.
+    //   Node newHead = reveseList(head.next);
+
+    //     // Rewire pointers:
+    //     // Make the next node (which is now the last node of the reversed sublist)
+    //     // point back to the current node.
+    //     head.next.next = head;
+
+    //     // Set the current node's next pointer to null to avoid a cycle,
+    //     // as it will be the last node in the reversed list from this point down.
+    //     head.next = null;
+
+    //     // Return the new head of the full reversed list.
+    //     return newHead;
+    // }
      public static Node reverseList(Node head) {
        Node q = null;
       Node p=head;
@@ -39,8 +60,11 @@ public class reverse {
         System.out.print("Linked List: ");
         printList(head);
 System.out.println();
-        Node r=reverseList(head);
+Node curr = head;
+        Node r=reverseList(curr);
+        // Node z=reveseList(head);
         printList(r);
+        //  printList(z);
 }
 
 }
